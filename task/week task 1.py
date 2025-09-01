98ee7cf2460422c5d5a3719d7be5988d8872f67c
Sunny <bsunnyash@gmail.com>
2025-09-01 17:56:45 +0530

    task 1

---
+++ task/week task 1.py
@@ -0,0 +1,10 @@
+principal=5000 
+rateofinterest=8.5/12
+time=18
+simpleinterest=(principal*rateofinterest*time)/100
+totalamt=principal+simpleinterest
+print("principal: ",principal,"$")
+print("rateofinterest:",rateofinterest,"%")
+print("time:",time,"months")
+print("simpleinterest:",simpleinterest,"$")
+print("totalamt:",totalamt,"$")
